ec4a0b832a314bedc1e0a549983694522bed12ae
Your Name <you@example.com>
2022-08-01 15:35:30 -0600

    commented code on W12D

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
-function LoginSuccess (response) {
-let login_body = document.getElementById(`login_body`).insertAdjacentHTML(`afterbegin`,
- `<h1>Successfully logged in</h1>`)
- Cookies.set(`Token`, `${response[`data`][`token`]}`)
- location.href = `home.html`
+function LoginSuccess(response) {                                              // following function will run if the api request is successful
+    document.getElementById(`login_body`).insertAdjacentHTML(                  // gets the id login body and inserts h1 tag in login body 
+        `afterbegin`,
+        `<h1>Successfully logged in</h1>`)
+    Cookies.set(`Token`, `${response[`data`][`token`]}`)                       // sets the cookie name to Token and the value to response (obj), data (obj), token(string),
+    location.href = `home.html`                                                // brings you to the home page
 }
 
 
-function LoginFail (error) {
-    let login_body = document.getElementById(`login_body`).insertAdjacentHTML(`afterbegin`,
-    `<h1>login was invalid</h1>`)
+function LoginFail(error) {                                                    // following function will run if the api request fails
+    document.getElementById(`login_body`).insertAdjacentHTML(                  // gets the id login body and inserts h1 tag in login body 
+        `afterbegin`,
+        `<h1>login was invalid</h1>`)
 }
 
-function AccountLogin (details) {
-
-let email = document.getElementById(`username`)[`value`];
-let password = document.getElementById(`password`)[`value`];
 
 
-axios.request({
+function AccountLogin(details) {                                               
 
-url: `https://reqres.in/api/login`,
-method: `POST`,
-data: {
-    email: email,
-    password: password
+    let email = document.getElementById(`username`)[`value`];                  // gets the id username and takes the value(whatever is typed in) and sets in as variable email
+    let password = document.getElementById(`password`)[`value`];               // gets the id password and takes the value(whatever is typed in) and sets in as variable password
+
+
+    axios.request({                                                            // axiox POST request that gets the api(url) and sends the email and password to a server (what it would do) 
+
+        url: `https://reqres.in/api/login`,
+        method: `POST`,
+        data: {
+            email: email,
+            password: password
+        }
+
+    }).then(LoginSuccess).catch(LoginFail)
 }
 
-}).then(LoginSuccess).catch(LoginFail)
-}
-
-let login_button = document.getElementById(`login_button`).addEventListener(`click`, AccountLogin)
+let login_button = document.getElementById(`login_button`).addEventListener(`click`, AccountLogin)  //gets the login_button id set as the variable login_button added an eventlistener that will run the function AccountLogin when clicked
+                                                                                                    
--- js/home.js
+++ js/home.js
@@ -1,11 +1,9 @@
-
-
 function colorsuccess(response) {
 
-let Token = Cookies.get(`Token`);
-if (Token !== undefined) {
-  for (let i = 0; i < response[`data`][`data`].length; i++) {
-    let home_section = document.getElementById(`home_section`).insertAdjacentHTML(`afterbegin`,
+let Token = Cookies.get(`Token`);                                                                   // gets the cookie Token and sets it as the vriable Token
+if (Token !== undefined) {                                                                          // if the Token is defined it will continue the code if it isnt it will go to the else 
+  for (let i = 0; i < response[`data`][`data`].length; i++) {                                       //set 0 as variable i, if i is less then the respone(obj), data(obj), data(array) lenth counter will increment by 1
+document.getElementById(`home_section`).insertAdjacentHTML(`afterbegin`,                            // gets the id home_section from html and inserts the Color, year and a color swatch by going into response, data, data, counter# and displaying whats called for
     `<h1>Welcome</h1>
     <h1>Color:${response[`data`][`data`][i][`name`]}</h1>
     <p>Year:${response[`data`][`data`][i][`year`]}</p>
@@ -14,37 +12,22 @@ if (Token !== undefined) {
     }
 }else{
 
-  let back_button = document.body.insertAdjacentHTML(`afterbegin`, `
-    <h1>you're not suppose to be here</h1>
+document.body.insertAdjacentHTML(`afterbegin`,                                                         //if the token doesnt exist it will insert a h1 tag abd a button for you to go back                                  
+  `<h1>you're not suppose to be here</h1>
     <button>back</button>`)
 
 }
 }
 
 
-function colorfail(error) {
-    let home_section = document.getElementById(`home_section`).insertAdjacentHTML(`afterbegin`,
+function colorfail(error) {                                                                             // if it fails the fetch the api it will insert a h1 tag on the page
+document.getElementById(`home_section`).insertAdjacentHTML(`afterbegin`,
         `<h1>couldnt load page</h1>`)
 }
 
 
-axios.request({
+axios.request({                                                                                       // requests the api thenwill go to success or fail function
 
     url: `https://reqres.in/api/unknown`
 
 }).then(colorsuccess).catch(colorfail)
-
-
-function logout (details) {
-
-let Token = Cookies.get(`Token`);
-if (Token !== undefined) {
-    document.body.insertAdjacentHTML(`afterbegin` `
-    <button id="logout button">Logout</button>
-
-    `)
-    Cookies.remove(`Token`);
-}
-
-}
-
